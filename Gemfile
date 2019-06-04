2b0b1ea62a4c6af32499d2e02254185bf1962f0a
Denali Balser <denali.balser167@gmail.com>
2019-06-04 19:14:46 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in compost_cli.gemspec
+gemspec
---
+++ lib/compost_cli.rb
@@ -0,0 +1,6 @@
+require "compost_cli/version"
+
+module CompostCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
