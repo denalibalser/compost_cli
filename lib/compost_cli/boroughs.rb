48d45f5cfc0dc74defd59800099dd3624971f9fa
Denali Balser <denali.balser167@gmail.com>
2019-06-06 19:43:47 +0000

    added compost_cli to my bin directory

--- lib/compost_cli/boroughs.rb
+++ lib/compost_cli/boroughs.rb
@@ -1,5 +1,18 @@
 class Boroughs 
-  attr_accessor :addresses 
+  attr_accessor :addresses, :collection_days
+  
+  @@all = []
+  
+  def initialize()
+    
+    
+    def initialize(name=nil, url=nil, location=nil, position=nil)
+    @name = name
+    @url = url
+    @location = location
+    @position = position
+    @@all << self
+  end
   
   
 end 
