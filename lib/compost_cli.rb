09d3893b0d4da12ccb279df0d80f0852a73e9aa0
Denali Balser <denali.balser167@gmail.com>
2019-06-06 23:52:09 +0000

    added dependency requirements to compost_cli.rb

--- lib/compost_cli.rb
+++ lib/compost_cli.rb
@@ -1,6 +1,8 @@
 require_relative "./compost_cli/version"
 require_relative './compost_cli/boroughs'
 require_relative './compost_cli/cli'
+require_relative './compost_cli/addresses'
+require_relative '.compost_cli/collection_days'
 
 module CompostCli
   class Error < StandardError; end
