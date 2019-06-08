17bcfd4c55f220e0357ed684498be0e2d5c7ca54
Denali Balser <denali.balser167@gmail.com>
2019-06-08 14:44:57 +0000

    added several methods to lib/cli.rb file

--- lib/compost_cli/boroughs.rb
+++ lib/compost_cli/boroughs.rb
@@ -3,6 +3,7 @@ class CompostCli::Boroughs
   
   def self.all 
     #should return a numbered array of bunch of instanes of boroughs 
+    #scrape data off of https://www.grownyc.org/compost/locations and then return the list of boroughs 
   end
   
   
--- lib/compost_cli/cli.rb
+++ lib/compost_cli/cli.rb
@@ -4,14 +4,15 @@ class CompostCli::CLI
   
   def call 
     puts "Welcome! Composting is an easy way to reduce waste. However, urban composting can be hard, we're here to help simplify it for New Yorkers!" 
-    #(1. Bronx, 2. Brooklyn, 3. Manhattan,  4. Queens, 5. Staten Island):
     list_boroughs 
     borough_selection
+    list_addresses
     address_selection
+    list_collection_days
     go_compost
     @boroughs = CompostCli::Boroughs.all
     @boroughs.each_with_index(1) do |borough, i|
-      puts "#{i}. #{borough.name}"
+      puts "#{i}. #{borough.name}: [@addresses]" #need this to return a list of the addresses associated with the selected borough 
     end 
     @addresses = CompostCli::Addresses.all 
     @addresses.each_with_index(1) do |address, i|
@@ -32,43 +33,53 @@ class CompostCli::CLI
   end 
   
   def borough_selection
-    puts "Please enter the number of the borough in which you would like to find a compost drop-off location:"
-    input = gets.strip
-    case input 
-    when "1"
-      puts "Addresses of compost drop-off locations in the Bronx:" 
-      #strip numbered list of Bronx addresses
-    when "2"
-      puts "Addresses of compost drop-off locations in Brooklyn:" 
-      #strip numbered list of Brooklyn addresses
-    when "3"
-      puts "Addresses of compost drop-off locations in Manhattan:" 
-      #strip numbered list of Manhattan addresses
-    when  "4" 
-      puts "Addresses of compost drop-off locations in Queens:" 
-      #strip numbered list of Queens addresses
-    when "5"
-      puts "Addresses of compost drop-off locations in Staten Island:" 
-      #strip numbered list of Staten Island addresses
+    input = nil 
+    while input != "exit"
+    puts "Please enter the number of the borough in which you would like to find a compost drop-off location or type exit to exit:"
+    input = gets.strip.downcase
+    
+    if input > 0 
+      the_borough = @boroughs[input.to_i(-1)]
+      puts "#{i}. #{borough.name}"
+    elsif input == "exit"
+      go_compost
+    else 
+      puts "Invalid Input. Please enter the number of the borough in which you would like to find a compost drop-off location or type exit to exit:"
     end 
+   end
+  end 
+  
+  def list_addresses 
+    #@addresses = [] (?)
+    puts "Here is the list of addresses for your borough selection:"
+    #scrape list of addresses for the address here and pushes into @addresses array
   end 
   
   def address_selection
     input = nil 
     while input != "exit" #starts loop (while input does not equal "exit" stay in the loop:)
     puts "Please enter the number of the compost drop-off address you would like more information on or type list to see the boroughs again or type exit to exit:"
-    inputs = gets.strip.downcase 
+    input = gets.strip.downcase 
     
-    if input.to > 0 #replaced when statement with this if statement 
-      puts @addresses[input.to_i-1]
+    if input.to > 0 #replaced when statements with this if statement 
+    the_borough = @boroughs[input.to_i(-1)]
+      puts "#{i}. #{borough.name}"
+      the_address = @addresses[input.to_i-1]
+      puts "#{i}. #{address.name}"
+      
     elsif input == "list"
       list_boroughs
     else 
       puts "Invalid Input. Please enter the number of the compost drop-off address you would like more information on or type list to see the boroughs again or type exit to exit:"
       
-    
     end 
    end #ends loop
+  end 
+  
+  def list_collection_days
+    #@collection_days =[]
+    puts "Here are the collection days and hours of operation for your address selection:"
+    #scrapes list of collection days and hours for the selected address and pushes into @collection_days array
   end 
   
   def go_compost
