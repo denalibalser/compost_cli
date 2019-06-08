39079bfa5b5bbd3afd81808fbed4e9c2f97897c4
Denali Balser <denali.balser167@gmail.com>
2019-06-08 19:40:53 +0000

    moved environment.rb into lib/compost_cli folder

--- lib/compost_cli/boroughs.rb
+++ lib/compost_cli/boroughs.rb
@@ -21,25 +21,7 @@ module CompostCli
   end 
     #should return a numbered array of bunch of instanes of boroughs 
     #scrape data off of https://www.grownyc.org/compost/locations and then return the list of boroughs 
-    
-
- 
   
-  def self.scrape_boroughs 
-    boroughs = []
-    #go to grownyc site, find the boroughs 
-    #extract the properties 
-    #instantiate a borough
-    #want to end up with an array of boroughs that I scraped 
-    boroughs << self.scrape_grownyc
-    
-    boroughs 
-  end 
-  
-  def self.scrape_grownyc
-    doc = Nokogiri::HMTL(open("https://www.grownyc.org/compost/locations"))
-    binding.pry
-  end 
   
   
 end 
---
+++ lib/compost_cli/environment.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require "compost_cli/version"
+require "compost_cli/addresses"
+require "compost_cli/boroughs"
+require "compost_cli/cli"
+require "compost_cli/collection_days"
+#require "compost_cli/scraper"
+
+module CompostCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
