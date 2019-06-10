13398176fccf21a02f23307398cc75fbf9a30a3e
Denali Balser <denali.balser167@gmail.com>
2019-06-10 12:58:41 +0000

    commented out line 21 in boroughs.rb

--- lib/boroughs.rb
+++ lib/boroughs.rb
@@ -18,7 +18,7 @@ class Borough
     borough_list = []
     doc = Nokogiri::HTML(open("https://www.grownyc.org/compost/locations"))
     binding.pry
-    boroughs = doc.search(strip boroughs from website)
+    #boroughs = doc.search(strip boroughs from website)
     #boroughs.each_with_index{|borough, index|
     #if index < 5 then
     #end
